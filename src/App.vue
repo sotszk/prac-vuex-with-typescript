437a8f3b7a627418b295ce5f2d7237167786228b
SotaSuzuki <3.3sota.suzuki@gmail.com>
2019-08-06 17:32:30 +0900

    add basic form component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,45 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <BasicForm :form="form" />
+    <div>
+      <p>{{ fullName }}</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import BasicForm from "./components/BasicForm.vue";
+
+export interface Form {
+  lastName: string | null;
+  firstName: string | null;
+}
+
+interface Data {
+  form: Form;
+}
 
 export default Vue.extend({
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    BasicForm
+  },
+  data(): Data {
+    return {
+      form: {
+        lastName: "",
+        firstName: ""
+      }
+    };
+  },
+  computed: {
+    fullName(): string {
+      return `${this.form.lastName} ${this.form.firstName}`;
+    }
   }
 });
 </script>
---
+++ src/components/BasicForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <fieldset>
+      <legend>{{ formTitle }}</legend>
+      <div>
+        <label>lastName:</label>
+        <input type="text" v-model="form.lastName" />
+      </div>
+
+      <div>
+        <label>firstName:</label>
+        <input type="text" v-model="form.firstName" />
+      </div>
+    </fieldset>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    form: {
+      type: Object,
+      required: true
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
